1edb49078d4729eaefbf1d15f8a69e1e8a7a29b4
Dimitris Christodoulou <dimitris.christodoulou.dev@gmail.com>
2017-02-09 17:17:33 +0200

    Created part of the generator
    Created other files

---
+++ lib/parser.py
@@ -0,0 +1,3 @@
+def get_file_contents(file):
+    with open('output.txt') as f:
+        return f.readlines()
---
+++ main.py
@@ -0,0 +1,21 @@
+from lib.generator import *
+import random
+
+
+generate_file()
+
+for row in a:
+    b = row.rstrip()
+    print(b.split('\t'))
+
+
+
+
+
+
+
+
+
+
+
+
