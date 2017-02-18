b0419723c6edc0391897802440ada8f4614cb374
Dimitris Christodoulou <dimitris.christodoulou.dev@gmail.com>
2017-02-18 21:10:42 +0200

    Remain to add gaussian

--- lib/generator.py
+++ lib/generator.py
@@ -1,2 +1,21 @@
+
+def get_commands(path):
+    try:
+        return open(path).read().splitlines()
+    except FileNotFoundError:
+        print('Not found : ' + path)
+
+def preferences():
+    try:
+        return int(input("Enter the percentage of loop statements 100: "))
+    except ValueError:
+        print('Please give a positive number')
+
+
 def generate_input_file():
-    pass
+    print(preferences())
+
+
+
+
+    commands = get_commands('assets/conf.txt')
