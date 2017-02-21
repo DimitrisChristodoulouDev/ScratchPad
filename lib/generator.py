9f9cc194ec218b3279691335c344c29cca14723b
Dimitris Christodoulou <dimitris.christodoulou.dev@gmail.com>
2017-02-21 19:33:52 +0200

    Final generator commit V1
    File nomizo en teliomeno
    Evala je tus elegxus gia to user input
    Kame to run molis to katevaseis na deiis

--- lib/generator.py
+++ lib/generator.py
@@ -1,7 +1,5 @@
 from random import *
-import numpy as np
-import scipy.stats
-from array import array
+import os
 
 COMMANDS_PATH = 'assets/conf.txt'  # Commands file path
 output_file = open('assets/output.txt', 'w')
@@ -9,41 +7,41 @@ output_file = open('assets/output.txt', 'w')
 
 def generate_input_file():
     get_preferences()  # 1.Get user input
-    get_commands() #6 Read commands filr
-    run_generation() #9. The main generator
+    get_commands()  # 6 Read commands filr
+    run_generation()  # 9. The main generator
 
 
 def push_to_file(row):
-    print(str(row), file=output_file)
+    print(prettify_row(row), file=output_file)
 
 
 def prettify_row(row):
-    pass
-    #return str(row['name'] + '\t' + str(row['cycles']) + '\t' + str(row['pc']))
+    return str(row['name'] + '\t' + str(row['cycles']) + '\t' + str(row['pc']) + '\t' + str(row['branch']) + '\t' + str(
+        row['loop']))
 
 
 def get_commands():
     try:
-        global commands_set #7. Make it global to access it from everywhere
-        commands_set = open(COMMANDS_PATH).read().splitlines() #8. Read the file into the array
+        global commands_set  # 7. Make it global to access it from everywhere
+        commands_set = open(COMMANDS_PATH).read().splitlines()  # 8. Read the file into the array
     except FileNotFoundError:
         print('Not found : ' + COMMANDS_PATH)
 
 
 def loop_flag():  # Selini
     var = uniform(0, 1)
-    return False # if var > 0.50 else False
+    return True  if var > 0.50 else False
 
 
-def commands_in_loop(): #11
+def commands_in_loop():  # 11
     return int(gauss(mean_branch, deviation_branch))
 
 
-def repeats_loop():#13
+def repeats_loop():  # 13
     return int(gauss(mean_rep_branch, deviation_rep_branch))
 
 
-def commands_in_serial(): #15
+def commands_in_serial():  # 15
     return int(gauss(mean_serial, deviation_serial))
 
 
@@ -55,14 +53,32 @@ def get_preferences():
     global mean_serial  # Mean number of serial commands
     global deviation_serial  # Standard deviation of serial commands
     try:
-        mean_branch = int(input("Enter the mean for branch commands: ")) #2
-        deviation_branch = int(input("Enter the standard deviation for branch commands: ")) #3
-        mean_rep_branch = int(input("Enter the mean for repeat times for branch blocks: ")) #4
-        deviation_rep_branch = int(input("Enter the standard deviation for repeat times for branch blocks: ")) #5
-        mean_serial = int(input("Enter the mean for serial commands: ")) #5
-        deviation_serial = int(input("Enter the deviation_serial: ")) #6
-    except ValueError: # Throw exception if input is not a number
-        print('Please give a number')
+        mean_branch = int(input("Enter the mean for branch commands: "))  # 2
+        deviation_branch = int(input("Enter the standard deviation for branch commands(smaller than "+str(mean_branch)+"): "))  # 3
+        if deviation_branch >= mean_branch or deviation_branch <= 0 or mean_branch <= 0 :
+            os.system('cls')
+            print('Invalid input...')
+            get_preferences()
+
+        mean_rep_branch = int(input("Enter the mean for repeat times for branch blocks: "))  # 4
+        deviation_rep_branch = int(input("Enter the standard deviation for repeat times for branch blocks (smaller than "+str(mean_rep_branch)+"): "))  # 5
+        if deviation_branch >= mean_branch or deviation_branch <= 0 or mean_branch <= 0 :
+            os.system('cls')
+            print('Invalid input...')
+            get_preferences()
+
+        mean_serial = int(input("Enter the mean for serial commands: "))  # 5
+        deviation_serial = int(input("Enter the deviation_serial(smaller than "+str(mean_serial)+"):: "))  # 6
+        if deviation_branch >= mean_branch or deviation_branch <= 0 or mean_branch <= 0 :
+            os.system('cls')
+            print('Invalid input...')
+            get_preferences()
+
+    except ValueError:  # Throw exception if input is not a number
+        print('Please give valid numbers')
+        os.system('cls')
+        print('Invalid input...')
+        get_preferences()
 
 
 def get_command():
@@ -70,58 +86,64 @@ def get_command():
 
 
 def print_header():
-    print(str("instruction_type" + '\t' + "cycles" + '\t' + "power consumption"), file = output_file)
+    print(str("instruction_type" + '\t' + "cycles" + '\t' + "power consumption"), file=output_file)
+
+
+def run_times():
+    try:
+        return int(input('How many blocks of code: '))
+    except ValueError:
+        print('Please give a number')
 
 
 def run_generation():
     print_header()
 
-    for count in range(0, 3):
-        print('Iteration', count)
+    for count in range(0, run_times()):
+        print('Iteration : ', count)
         num_of_com_loop = commands_in_loop()
         num_of_serial = commands_in_serial()
         num_of_loop_rep = repeats_loop()
-
-        print('Flag', loop_flag())
+        flag = loop_flag()
+        print('Flag', flag)
         print('Loop', num_of_com_loop)
         print('Repeat', num_of_loop_rep)
         print('Serial', num_of_serial)
-
-
-
-
-
-        my_arr = []
-        if loop_flag():
-            #run loop
-            for i in range(0, num_of_com_loop):
-                my_arr.append(get_command())
-                # name = command[0]
-                # cycles = command[1]
-                # power_consumption = command[2]
-            for x in range(0, num_of_loop_rep):
-                for y in my_arr:
-                   if x==0:
-                       y = y+'\t1'+'\t0'
-                   else:
-                       y=y+'\t1'+'\t1'
-                   push_to_file(y)
-                   print('Serial')
-                   for i in range(0, num_of_serial):
-                       push_to_file(get_command())
+        if flag:  # Run loop
+            run_loop(num_of_com_loop, num_of_loop_rep)
         else:
-            print('Serial')
-            for i in range(0, num_of_serial):
-                push_to_file(get_command()+'\t0'+'\tx')
-            for i in range(0, num_of_com_loop):
-                my_arr.append(get_command())
-                # name = command[0]
-                # cycles = command[1]
-                # power_consumption = command[2]
-            for x in range(0, num_of_loop_rep):
-                for y in my_arr:
-                    if x == 0:
-                        y = y + '\t1' + '\t0'
-                    else:
-                        y = y + '\t1' + '\t1'
-                    push_to_file(y)
+            pass
+            run_serial(num_of_serial)
+    output_file.close()
+    print('Opening file...')
+    osCommandString = "notepad.exe assets/output.txt"
+    os.system(osCommandString)
+
+
+def run_loop(num_of_com_loop, num_of_loop_rep):
+    print('Loop')
+    for j in range(0, num_of_loop_rep):
+        for i in range(0, num_of_com_loop):
+            command = get_command().split('\t')
+            obj = {
+                'name': command[0],
+                'cycles': command[1],
+                'pc': command[2],
+                'branch': '1' if j == 0 else 1,
+                'loop': '0' if j == 0 else 1
+            }
+            push_to_file(obj)
+
+
+def run_serial(num_of_serial):
+    print('Serial')
+    for i in range(0, num_of_serial):
+        command = get_command().split('\t')
+        obj = {
+            'name': command[0],
+            'cycles': command[1],
+            'pc': command[2],
+            'branch': '0',
+            'loop': 'x'
+        }
+        push_to_file(obj)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from lib.commands import *
 from lib.generator import *
 
+os.system('cls')
 generate_commands()  # To create the file with the commands
 generate_input_file()  # Create the input file to use in the parser
